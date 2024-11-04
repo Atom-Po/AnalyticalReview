ad0860c2796247d6b8216dda59638193059276b6
Atom-Po <wwwpromised@gmail.com>
2024-11-04 23:12:05 +0900

    drafting

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import FS_scrapper
+import FS_crawler
 import stock_searcher
 
 if __name__ == '__main__':
     find_name, find_code = stock_searcher.find_similar_code('삼성전자')
     print(f'find_name : {find_name}, find_code : {find_code}')
-    fs_df = FS_scrapper.FS_crawler(find_code)
+    fs_df = FS_crawler.FS_crawler(find_code)
     print(fs_df)
